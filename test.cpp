82953ab015f8cbeec5947f13c01f1f12fe302076
apple-ba <17702115812@189.cn>
2021-02-03 20:42:05 +0800

    Add a test for the program

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<fstream> 
+#include<ctime>
+using namespace std;
+int main() {
+	cout << "测试系统已被启动..." << endl;
+	cout << "按任意键以开始测试文件读入..." << endl;
+	system("pause");
+	system("cls");
+	cout << "正在测试输入文件(test.in)..." << endl;
+	ifstream fin("test.in");
+	char ch;
+	while (fin >> ch)cout << ch;
+	cout << endl; 
+	cout << "已完成文件输入测试(test.in)..." << endl;
+	cout << "按任意键以开始测试文件输出..." << endl;
+	system("pause");
+	system("cls");
+	cout << "开始测试输出(test.out)..." << endl;
+	ofstream fout("test.out");
+	int a = time(NULL);
+	fout << a << endl;
+	cout << "请在测试程序结束后检查test输出是否与以下数值相等..." << endl;
+	cout << a << endl;
+	cout << "测试已结束" << endl;
+	cout << "请在此程序结束后检查i/o文件是否删除!" << endl;
+	system("pause");
+	return 0;
+
+}
